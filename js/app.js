278fd03d9fdd50244739ab3cbcb91f4a0fc3fef3
Barisdemirci3 <baris19052003@gmail.com>
2024-03-22 16:08:59 +0300

    Files updated and sweetaler2 included.

--- js/app.js
+++ js/app.js
@@ -1,49 +1,72 @@
-const API_KEY = "32fb26b56705b2e631c63ce39ec84aff";
-
-function KelvinToCelsius(kelvin) {
-    return kelvin - 273.15;
+function error(text) {
+    Swal.fire({
+        title: 'Hata!',
+        text: `${text}`,
+        icon: 'error',
+        confirmButtonText: 'Tamam'
+    })
 }
 
+const API_KEY = "32fb26b56705b2e631c63ce39ec84aff";
+var JsonData = {};
+function KelvinToCelsius(kelvin) {
+    return Math.floor(kelvin - 273.15);
+}
 function GetWeatherData(city) {
-    return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("API'YE BAĞLANILAMADI!");
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`).then((response) => {
+        if (!response.status == 200) {
+            throw new Error("API'a bağlanılamadı");
+        }
+        return response.json()
+    }).then((data) => {
+        let lat, lon;
+        lat = data[0].lat
+        lon = data[0].lon
+        return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=tr`).then((response) => {
+            if (!response.status == 200) {
+                throw new Error("API'a bağlanılamadı");
             }
             return response.json();
-        }).then((geoData) => {
-            const lat = geoData[0].lat;
-            const lon = geoData[0].lon;
-
-            return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=tr`);
-        }).then((weatherResponse) => {
-            if (!weatherResponse.ok) {
-                throw new Error("API'YE BAĞLANILAMADI!");
-            }
-            return weatherResponse.json();
-        }).then((weatherData) => {
-            const description = weatherData.current.weather[0].description;
-            const temp = KelvinToCelsius(weatherData.current.temp);
-            const humidity = weatherData.current.humidity;
-            return {
-                description,
-                temp,
-                humidity
-            };
-        }).catch((error) => {
-            console.error(error);
-            return null;
-        });
+        }).then((data) => {
+            return data;
+        })
+    }).catch((er) => {
+        error("Lütfen geçerli bir şehir ismi giriniz!");
+        throw new Error(er);
+    })
 }
-
-$("#weather_app").submit((e) => {
+$("#weather_app").submit((e, city) => {
     e.preventDefault();
-    const city = $("#city").val().trim();
-    if (city === "") {
-        alert("Lütfen bir şehir ismi giriniz!");
-        return;
+    city = $("#city").val().trim();
+    if (city == "") {
+        return error("Hava durumunu sorgulamak için şehir kısmını doldurmalısınız!");
     }
     GetWeatherData(city).then((data) => {
         console.log(data);
+        JsonData = {
+            temp: KelvinToCelsius(data.current.temp),
+            humidty: data.current.humidity,
+            wind_speed: data.current.wind_speed,
+            weather_description: data.current.weather[0].description,
+            icon: data.current.weather[0].icon
+        }
+        $('#weather-info').removeClass('d-none'); // 'd-none' sınıfını kaldır
+        $('#weather-info').addClass('d-block');
+        console.log(JsonData)
+        var city_name = $('#city_name');
+        var temp = $('#temperature');
+        var desc = $('#description');
+        var humidity = $('#humidity');
+        var wind_speed = $('#wind-speed');
+        var weather_icon = $('#weather-icon');
+        city_name.text(city);
+        temp.text(JsonData.temp);
+        desc.text(JsonData.weather_description);
+        humidity.text(JsonData.humidty);
+        wind_speed.text(JsonData.wind_speed);
+        weather_icon.attr('src', `https://openweathermap.org/img/wn/${JsonData.icon}@4x.png`);
+
+
     });
 });
+
