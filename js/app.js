6ff9a7f9ee3b176cd9e4da56c4ffff4253351da5
Barisdemirci3 <baris19052003@gmail.com>
2024-03-26 00:32:47 +0300

    Functions edited and optimized.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const API_KEY = "32fb26b56705b2e631c63ce39ec84aff";
 var JsonData = {};
-
 function error(text) {
     Swal.fire({
         title: 'Hata!',
@@ -21,26 +20,30 @@ function ShowDatas() {
         var humidity = $('#humidity');
         var wind_speed = $('#wind-speed');
         var weather_icon = $('#weather-icon');
-        city_name.text(city);
+        city_name.text(JsonData.city);
         temp.text(JsonData.temp);
         desc.text(JsonData.weather_description);
         humidity.text(JsonData.humidty);
         wind_speed.text(JsonData.wind_speed);
         weather_icon.attr('src', `https://openweathermap.org/img/wn/${JsonData.icon}@4x.png`);
   }
-function GetWeatherFromGeo(lat,lon){
+function GetWeatherFromGeo(lat,lon,city){
     return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then((response) => {
         if(response.status != 200){
             throw new Error('Can not connected to API!');
         }
         return response.json();
     }).then((data) => {
+        if(city == ""){
+            city = data.timezone;
+        }
        JsonData = {
             temp: KelvinToCelsius(data.current.temp),
             humidty: data.current.humidity,
             wind_speed: data.current.wind_speed,
             weather_description: data.current.weather[0].description,
-            icon: data.current.weather[0].icon
+            icon: data.current.weather[0].icon,
+            city: city
         }
         ShowDatas();
     }).catch((er) => {
@@ -55,25 +58,27 @@ function GetCityGEO(city){
         return response.json()
     })
 }
+function GetWeatherData(city) {
+    GetCityGEO(city).then((data) => {
+         let lat, lon;
+         lat = data[0].lat
+         lon = data[0].lon
+         GetWeatherFromGeo(lat,lon,city)
+     }).catch((er) => {
+         error("Please enter a valid city name.")
+         throw new Error(er);
+     })
+ }
 $('#use_current_location').click(() => {
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition((positions) => {
         lat = positions.coords.latitude;
         lon = positions.coords.longitude;
-        GetWeatherFromGeo(lat,lon);
+        GetWeatherFromGeo(lat,lon,"");
     });
 }
 })
-function GetWeatherData(city) {
-   GetCityGEO(city).then((data) => {
-        let lat, lon;
-        lat = data[0].lat
-        lon = data[0].lon
-        console.log(lat,lon)
-    })
-}
-$("#weather_app").submit((e, city) => {
-    e.preventDefault();
+$("#weather_app").submit((city) => {
     city = $("#city").val().trim();
     if (city == "") {
         return error("Please enter a city name!");
