6fd16cc6ad102befa1a87bb71b8bbbda6abb5d6b
Barisdemirci3 <baris19052003@gmail.com>
2024-03-26 00:19:42 +0300

    User current location weather added.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const API_KEY = "32fb26b56705b2e631c63ce39ec84aff";
+var JsonData = {};
+
 function error(text) {
     Swal.fire({
         title: 'Hata!',
@@ -6,50 +9,11 @@ function error(text) {
         confirmButtonText: 'Tamam'
     })
 }
-
-const API_KEY = "32fb26b56705b2e631c63ce39ec84aff";
-var JsonData = {};
 function KelvinToCelsius(kelvin) {
     return Math.floor(kelvin - 273.15);
 }
-function GetWeatherData(city) {
-    return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`).then((response) => {
-        if (!response.status == 200) {
-            throw new Error("API not connected.");
-        }
-        return response.json()
-    }).then((data) => {
-        let lat, lon;
-        lat = data[0].lat
-        lon = data[0].lon
-        return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then((response) => {
-            if (!response.status == 200) {
-                throw new Error("API not connected.");
-            }
-            return response.json();
-        }).then((data) => {
-            return data;
-        })
-    }).catch((er) => {
-        error("Please enter a valid city name.!");
-        throw new Error(er);
-    })
-}
-$("#weather_app").submit((e, city) => {
-    e.preventDefault();
-    city = $("#city").val().trim();
-    if (city == "") {
-        return error("Please enter a city name!");
-    }
-    GetWeatherData(city).then((data) => {
-        JsonData = {
-            temp: KelvinToCelsius(data.current.temp),
-            humidty: data.current.humidity,
-            wind_speed: data.current.wind_speed,
-            weather_description: data.current.weather[0].description,
-            icon: data.current.weather[0].icon
-        }
-        $('#weather-info').removeClass('d-none'); // 'd-none' sınıfını kaldır
+function ShowDatas() {
+    $('#weather-info').removeClass('d-none'); // 'd-none' sınıfını kaldır
         $('#weather-info').addClass('d-block');
         var city_name = $('#city_name');
         var temp = $('#temperature');
@@ -63,8 +27,56 @@ $("#weather_app").submit((e, city) => {
         humidity.text(JsonData.humidty);
         wind_speed.text(JsonData.wind_speed);
         weather_icon.attr('src', `https://openweathermap.org/img/wn/${JsonData.icon}@4x.png`);
-
-
+  }
+function GetWeatherFromGeo(lat,lon){
+    return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then((response) => {
+        if(response.status != 200){
+            throw new Error('Can not connected to API!');
+        }
+        return response.json();
+    }).then((data) => {
+       JsonData = {
+            temp: KelvinToCelsius(data.current.temp),
+            humidty: data.current.humidity,
+            wind_speed: data.current.wind_speed,
+            weather_description: data.current.weather[0].description,
+            icon: data.current.weather[0].icon
+        }
+        ShowDatas();
+    }).catch((er) => {
+        throw new Error(er);
+    })
+}
+function GetCityGEO(city){
+    return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`).then((response) => {
+        if (!response.status == 200) {
+            throw new Error("API not connected.");
+        }
+        return response.json()
+    })
+}
+$('#use_current_location').click(() => {
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((positions) => {
+        lat = positions.coords.latitude;
+        lon = positions.coords.longitude;
+        GetWeatherFromGeo(lat,lon);
     });
+}
+})
+function GetWeatherData(city) {
+   GetCityGEO(city).then((data) => {
+        let lat, lon;
+        lat = data[0].lat
+        lon = data[0].lon
+        console.log(lat,lon)
+    })
+}
+$("#weather_app").submit((e, city) => {
+    e.preventDefault();
+    city = $("#city").val().trim();
+    if (city == "") {
+        return error("Please enter a city name!");
+    }
+    GetWeatherData(city);
 });
-
